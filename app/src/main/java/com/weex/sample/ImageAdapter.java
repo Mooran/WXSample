571d30bfd7f2d8efb05a4bf79c1cd1bf6a5da1fa
mooran <mooran953@gmail.com>
2017-03-13 15:59:47 +0800

    使用glide实现下载图片接口

--- app/src/main/java/com/weex/sample/ImageAdapter.java
+++ app/src/main/java/com/weex/sample/ImageAdapter.java
@@ -2,6 +2,8 @@ package com.weex.sample;
 
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.taobao.weex.WXEnvironment;
 import com.taobao.weex.adapter.IWXImgLoaderAdapter;
 import com.taobao.weex.common.WXImageStrategy;
 import com.taobao.weex.dom.WXImageQuality;
@@ -15,5 +17,6 @@ public class ImageAdapter implements IWXImgLoaderAdapter {
   @Override
   public void setImage(String url, ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
     //实现你自己的图片下载。
+    Glide.with(WXEnvironment.getApplication()).load(url).into(view);
   }
 }
